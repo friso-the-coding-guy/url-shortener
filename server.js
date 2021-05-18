54a8a296a9a987cb5a6d6dcb5ab49657a41069e8
Friso <>
2021-05-18 09:26:42 +0200

    added express

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send({
+        message: "Hello World"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
